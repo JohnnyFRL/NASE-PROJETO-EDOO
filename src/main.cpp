1fcd7cb7969a682ccc25cf4beb71e8e4ac23bf3f
Johnny Cleiton Ferreira lins <122063883+JohnnyFRL@users.noreply.github.com>
2026-03-27 23:24:24 -0300

    funcionario (menu,cadastro e historico)

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,76 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include "Pessoa.hpp"
 #include "Paciente.hpp"
-using namespace std;
 
-int main() {
-    Paciente p("vitor", 25, "123.456.789-00", "987654321", "Rua Exemplo, 123", "Sem histórico médico", "Engenharia", "vitor@example.com", true, false);
-    cout << p.getPaciente() << endl;
-    cout << "Status do paciente: " << p.getStatus() << endl;
-    p.validar();
-    cout << "Status do paciente apos validacao: " << p.getStatus() << endl;
+using namespace std;
+void exibirMenuFuncionario();
+void cadastrarPaciente(vector<Paciente>& lista);
+
+int main(){
+    vector<Paciente> listaPacientes;
+    int opcao;
+
+    do{
+        exibirMenuFuncionario();
+        cin >> opcao;
+        cin.ignore();
+
+        if (opcao == 1){
+            cadastrarPaciente(listaPacientes);
+
+        } else if (opcao == 2){
+            if (listaPacientes.empty()){
+                cout << "\nNenhum paciente cadastrado." << endl;
+            } else {
+                cout << "\n--- Lista de Pacientes ---" << endl;
+                for (size_t i = 0; i < listaPacientes.size(); i++) {
+                    cout << i + 1 << ". " << listaPacientes[i].getPaciente() << endl;
+                    cout << "   Status: " << listaPacientes[i].getStatus() << endl;
+                    cout << "---------------------------" << endl;
+                }
+            }
+        }
+    }while (opcao != 0);
+
+    cout << "Saindo do sistema..." << endl;
     return 0;
+}
+
+void exibirMenuFuncionario(){
+    cout << "\n--- Portal do Atendente ---" << endl;
+    cout << "1. Cadastrar aluno" << endl;
+    cout << "2. ver historico" << endl;
+    cout << "0. Sair" << endl;
+    cout << "Escolha uma opcao: ";
+}
+
+void cadastrarPaciente(vector<Paciente>& lista){
+    string nome, cpf, telefone, endereco, historico, curso, email;
+    int idade;
+    char souA, souB;
+    bool alunoUFPE, bolsistaPROAES;
+
+    cout << "\n--- Cadastro de Paciente ---" << endl;
+    cout << "Nome: "; getline(cin, nome);
+    cout << "Idade: "; cin >> idade; cin.ignore();
+    cout << "CPF: "; getline(cin, cpf);
+    cout << "Telefone: "; getline(cin, telefone);
+    cout << "Endereco: "; getline(cin, endereco);
+    cout << "Historico Medico: "; getline(cin, historico);
+    cout << "Curso: "; getline(cin, curso);
+    cout << "Email: "; getline(cin, email);
+    
+    cout << "E aluno da UFPE? (s/n): "; cin >> souA;
+    alunoUFPE = (souA == 's' || souA == 'S');
+
+    cout << "E bolsista PROAES? (s/n): "; cin >> souB;
+    bolsistaPROAES = (souB == 's' || souB == 'S');
+
+    Paciente novoPaciente(nome, idade, cpf, telefone, endereco, historico, curso, email, alunoUFPE, bolsistaPROAES);
+    novoPaciente.validar();
+    lista.push_back(novoPaciente);
+    
+    cout << "\n[Sucesso] Paciente cadastrado e validado!" << endl;
 }
